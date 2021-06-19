7eeff132f50843da66d1cdad72dfe305d317daea
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2021-06-19 07:13:29 +0100

    added static file, template layouts based rendering

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
 	log.Println("starting server on port ..5050")
 	err := http.ListenAndServe(":5050", mux)
 	log.Fatal(err)
