c1491ed800bf81265d32c2ecc27c0c975f396d2a
Neeraj Sidhaye <bethecodewithyou@gmail.com>
2021-06-30 09:07:20 +0100

    adding centralized error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,7 +11,7 @@ import (
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	app.InfoLog.Println("home called")
@@ -25,25 +25,22 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if err!=nil {
 
 		app.ErrorLog.Println(err.Error())
-		http.Error(w, "internal server error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err!=nil {
 		app.ErrorLog.Println(err.Error())
-		http.Error(w, "internal server error", 500)
+		app.serverError(w, err)
 	}
-
-
-	//w.Write([]byte("hello from Snippetbox"))
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 
 	id, err:= strconv.Atoi(r.URL.Query().Get("id"))
 	if err!=nil || id<1{
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -54,7 +51,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "method not method", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
